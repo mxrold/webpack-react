706839aa6c36133c4e8d0e8bef61bc52c26aaeba
Martin <martinroldan889@gmail.com>
2021-08-23 19:46:27 -0300

    :heavy_plus_sign: [Add] Html Loader and Html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
@@ -7,7 +8,7 @@ module.exports = {
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['.js', ',jsx']
+    extensions: ['.js', '.jsx']
   },
   module: {
     rules: [
@@ -17,9 +18,21 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader'
+        }
       }
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html'
+    })
+  ],
   devServer: {
     static: path.join(__dirname, 'dist'),
     compress: true,
